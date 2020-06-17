263606478cb5d4635aff2a272589195804463de3
Santiago Castillo <sancastillog@gmail.com>
2020-06-17 01:00:31 +0000

    empty and 1 sized list

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,6 +12,8 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i = 0, j = 0;
 
+	if (!array || size <= 1)
+		return;
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
